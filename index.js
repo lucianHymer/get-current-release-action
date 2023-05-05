2cc171f43ae39befa756dcce821c6c8ae55b29d3
Lucian Hymer <lucian@lucianhymer.com>
2023-05-05 15:01:11 -0700

    using repos api

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  const octokit = new github.Octokit({
-    auth: core.input("token"),
-  });
-  const releases = await octokit.request(
-    "GET /repos/{owner}/{repo}/releases?per_page=2&page=1",
-    {
-      owner: "lucianHymer",
-      repo: "workflowTest",
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    }
-  );
-
-  // core.setOutput("tag", "abc");
-  console.log(`The release payload: ${releases}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const octokit = new github.Octokit({
+      auth: core.input("token"),
+    });
+    const owner = "lucianHymer";
+    const repo = "workflowTest";
+    var releases = await octokit.repos.listReleases({
+      owner: owner,
+      repo: repo,
+    });
+    // core.setOutput("tag", "abc");
+    console.log(`The release payload: ${releases}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
